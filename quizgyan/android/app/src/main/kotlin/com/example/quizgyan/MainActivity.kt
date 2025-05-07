a02ea187b1a7fa56002d437b711dcf7c03fd703e
Himal bhusal <mememimi413@gmail.com>
2025-05-07 15:09:16 +0545

    initial commit

---
+++ quizgyan/android/app/src/main/kotlin/com/example/quizgyan/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.quizgyan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ quizgyan/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:quizgyan/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Quiz Gyan',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
